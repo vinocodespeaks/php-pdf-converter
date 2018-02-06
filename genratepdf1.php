a9585b470c29984c711eac3a9da7ed4f705b282c
vinodcodespeaks <vinod.looser00@gmail.com>
2018-02-07 00:40:22 +0530

    initial commit

---
+++ genratepdf1.php
@@ -0,0 +1,13 @@
+<?php
+require_once("config.php");
+require("fpdf.php");
+$pdf = new FPDF();
+$pdf->AddPage();
+
+
+
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'podu thakida thakida pdf generated vinodhey..................!');
+$pdf->Output();
+?>
+
